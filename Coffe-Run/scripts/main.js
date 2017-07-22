f68dc1c0ca80139b3015e4b95b712e4124036dc4
hksix <mh.haseeb@gmail.com>
2017-07-22 14:52:48 -0400

    functioning coffeeRun with only Jquery and LocalStorgae

--- Coffe-Run/scripts/main.js
+++ Coffe-Run/scripts/main.js
@@ -1,7 +1,7 @@
 var $form = $('[data-coffee-order="form"]');
 var $coffeeOrder =$('[data-role="order"]');
 var $emailAddress =$('[data-role="email"]');
-var $coffeeSize =$('[data-role="size"]');
+// var $coffeeSize =$('input[data-role="size"]');
 var $shotFlavor =$('[data-role="flavor"]');
 var $strengthLevel=$('[data-role="strengthLevel"]');
 
@@ -9,16 +9,22 @@ $form.on('submit', function (event){
     event.preventDefault();
     localStorage.setItem('coffeeOrder', $coffeeOrder.val());
     localStorage.setItem('emailAddress', $emailAddress.val());
-    localStorage.setItem('coffeeSize', $coffeeSize.val());
+    localStorage.setItem('coffeeSize', $('input[data-role="size"]:checked').val());
     localStorage.setItem('shotFlavor', $shotFlavor.val());
     localStorage.setItem('strengthLevel', $strengthLevel.val());
 });
 
 
-$form.on('load', function (event) {
+$(document).ready(function () {
+    console.log("something");
     $coffeeOrder.val(localStorage.getItem('coffeeOrder'));
-    $emailAddress.val(localStorage.getItem('emialAddress'));
-    $coffeeSize.val(localStorage.getItem('coffeeSize'));
+    $emailAddress.val(localStorage.getItem('emailAddress'));
+    $('input:radio[data-role="size"]').filter('[value=' + localStorage.getItem('coffeeSize') + ']').prop('checked', true);
     $shotFlavor.val(localStorage.getItem('shotFlavor'));
     $strengthLevel.val(localStorage.getItem('strengthLevel'));
 });
+
+// function getSize(){
+//    return $coffeeSize = $('input[data-role="size"]:checked').val();
+
+// }
