00ca79a7c6195d87ae34b88fbc2c35ef173bb860
hksix <mh.haseeb@gmail.com>
2017-07-26 09:48:57 -0400

    correcting mutiple button issue

--- MovieApp/main.js
+++ MovieApp/main.js
@@ -4,7 +4,8 @@ var $form = $('[data-search="form"]');
 var $search = $('[data-role="search"]');
 var info = [];
 var theData;
-$form.on('submit', function (event){
+
+$(document).on('submit', function (event){
     event.preventDefault();
     var x = getInfo();
     x.then( function (data){
@@ -13,9 +14,6 @@ $form.on('submit', function (event){
         })   
     })
 
-function main(){
-    getData();
-        }
 
 function getInfo(){
     return $.get(OMDbAPI + $search.val());
@@ -25,7 +23,7 @@ function getData(info){
     var x = $.get("http://www.omdbapi.com/?apikey=1ca32dee&i=" + info);
     x.then(function (data){
         theData = data;
-        collaspsibleButton(theData);
+        divMaker(theData);
         moviePoster(theData);
     });
 }
@@ -33,68 +31,62 @@ function getData(info){
 
 function moviePoster(data){
     var posterSrc = data.Poster;
-    imgMaker(posterSrc);
+    return posterSrc;
+
 }
 
-function divMaker(){
-    var $div = $("<div>", {"class": "moviePoster"});
-    $div.click(function( event){
-        event.preventDefault;
-    });
-    
-    $('#box').append($div);
+
+function divMaker(data){
+
+    var movieTitle = data.Title;
+    movieTitle = movieTitle.replace(/\s+/g, '');
+    var divId = "#"+movieTitle;
+    divId = divId.replace(/\s+/g, '');
+    var mainDataRole = "[data-role-main='"+movieTitle+"']"
+    var $div = $("<div>", {"class": "Container", "data-role-main":movieTitle});
+    $div.css({"display":"flex","flex-direction":"column","position":'relative'});
+    $('body').append($div);
+    movieInfo(data,divId,movieTitle);
+    imgMaker(moviePoster(data),mainDataRole);
 }
 
-function imgMaker(imgSrc){
+function imgMaker(imgSrc,id){
     var $img = $('<img>' ,{"src": imgSrc});
     $img.css({'width': "10%", 'height':'10%','transition': 'height 3s', 'transition-delay':'1s'});
     $img.click(function (){
         $(this).animate({width:"40%"});
         });
-    // $img.click(function (){
-    //     $(this).animate({width:"10%"});
-    //     });
-    
-    $(".Container").append($img);
-
+    $(id).append($img);
 }
 
-function collaspsibleButton(data){
-    var $divC = $("<div>", {"class": "Container"});
-    $divC.css({"display":"flex","flex-direction":"column"});
+function movieInfo(data,name,movieTitle){
     var $h2 = $("<h2>", {"title": "movie"});
     $h2.text(data.Title);
 
-    var $buttonInfo = $('<a>',{"class":"btn btn-info", "data-toggle":"collapse"});
-    $buttonInfo.attr('href', "#demo"); 
+    var $buttonInfo = $('<a>',{"class":"btn btn-info","data-role-button":movieTitle, "data-toggle":"collapse"});
+    $buttonInfo.attr('href', name); 
     $buttonInfo.text(data.Released);
     $buttonInfo.css({"display:":"inline-block",'width': "40%", 'height':'40%','float':'right'});
 
-    var $collapseContent = $("<div>", {"id": "demo", "class":"collapse"});
-    // $collapseContent.text(dataExtractor(data));
-    $collapseContent.css({'white-space':"normal"/*'width': "100%", 'height':'100%'*/});
+    var $collapseContent = $("<div>", {"id": movieTitle, "class":"collapse","data-role-collapse": movieTitle});
+    $collapseContent.css({'white-space':"normal"});
     var $para =$("<p>");
     $para.text(dataExtractor(data));
-    $para.css({});
 
-    $('#box').append($divC);
-    $('.Container').append($h2);
-    $('.Container').append("<p>Click on the button to toggle between showing and hiding content.</p>");
-    $('.Container').append($buttonInfo);
-    $('.btn-info').append($collapseContent);
-    $('.collapse').append($para);
-
+    $("[data-role-main='"+movieTitle+"']").append($h2);
+    $("[data-role-main='"+movieTitle+"']").append("<p>Click on the button to toggle between showing and hiding content.</p>");
+    $("[data-role-main='"+movieTitle+"']").append($buttonInfo);
+    $("[data-role-button='"+movieTitle+"']").append($collapseContent);
+    $("[data-role-collapse='"+movieTitle+"']").append($para);
 }
 
 
 function dataExtractor(data){
     var info;
-    var $lineBr = $("<br>");
-    var $para =$("<p>");
     var keys = Object.keys(theData);
     var values = Object.values(theData);
     for (i in keys){
-        // $para.text((keys[i] +': ' +values[i]) +("\n") +("\n"));
        info +=("\n")+keys[i] +("\n")+': ' +values[i] +("\n");
-}return info;
+        }
+    return info;
 }
